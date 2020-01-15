89c8922d4ff645379f6a0b1b8f6d8f66424575b0
Michael Walker <mike@barrucadu.co.uk>
2020-01-15 22:10:33 +0000

    Implement a basic SAT solver

--- src/lib.rs
+++ src/lib.rs
@@ -138,6 +138,7 @@ struct Model(Vec<(Literal, Provenance)>);
 enum Provenance {
     UnitPropagation,
     Decision,
+    Backjump,
 }
 
 impl Model {
@@ -182,5 +183,192 @@ impl Model {
         }
 
         has_positive.intersection(&has_negative).count() == 0
+    }
+}
+
+fn do_backjump(model: &mut Model) -> bool {
+    // this is a simpler version than in the paper (what it calls
+    // "standard backtracking") because I'm not sure how to implement
+    // `F |= C \/ l'`
+    let Model(lits) = model;
+
+    while let Some((lit, provenance)) = lits.pop() {
+        if provenance == Provenance::Decision {
+            model.append(lit.negate(), Provenance::Backjump);
+            return true;
+        }
+    }
+
+    false
+}
+
+fn do_unit_propagation(model: &mut Model, formula: &Formula) -> bool {
+    let Formula(clauses) = formula;
+
+    for clause in clauses {
+        if clause.is_true_in(model) == None {
+            let Clause(lits) = clause;
+
+            for lit in lits {
+                if lit.is_true_in(model) == None {
+                    let mut lits_without_lit = lits.clone();
+                    lits_without_lit.remove(lit);
+
+                    if Clause(lits_without_lit).is_true_in(model) == Some(false) {
+                        model.append(*lit, Provenance::UnitPropagation);
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    false
+}
+
+fn do_decision(model: &mut Model, formula: &Formula) -> bool {
+    let Formula(clauses) = formula;
+
+    for clause in clauses {
+        if clause.is_true_in(model) == None {
+            let Clause(lits) = clause;
+
+            for lit in lits {
+                if lit.is_true_in(model) == None {
+                    // make the lit positive
+                    model.append(Literal::new(lit.atom), Provenance::Decision);
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
+}
+
+fn dpll(formula: Formula) -> Option<Model> {
+    let mut model = Model::new();
+
+    loop {
+        match formula.is_true_in(&model) {
+            Some(true) => break,
+            Some(false) => {
+                if do_backjump(&mut model) {
+                    continue;
+                }
+
+                return None;
+            }
+            None => {
+                if do_unit_propagation(&mut model, &formula) {
+                    continue;
+                }
+                if do_decision(&mut model, &formula) {
+                    continue;
+                }
+
+                panic!("failed to do either unit propagation or decision in an incomplete model");
+            }
+        }
+    }
+
+    Some(model)
+}
+
+fn sat(formula: Formula) -> bool {
+    dpll(formula).is_some()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_sat_1() {
+        assert!(sat(mk_formula(&mut vec![mk_clause(&mut vec![1])])));
+    }
+
+    #[test]
+    fn simple_sat_2() {
+        assert!(sat(mk_formula(&mut vec![mk_clause(&mut vec![1, 2])])));
+    }
+
+    #[test]
+    fn simple_sat_2b() {
+        assert!(sat(mk_formula(&mut vec![
+            mk_clause(&mut vec![-1]),
+            mk_clause(&mut vec![1, -2]),
+        ])));
+    }
+
+    #[test]
+    fn simple_sat_3() {
+        assert!(sat(mk_formula(&mut vec![
+            mk_clause(&mut vec![1, 2]),
+            mk_clause(&mut vec![3])
+        ])));
+    }
+
+    #[test]
+    fn simple_unsat_1() {
+        assert!(!sat(mk_formula(&mut vec![
+            mk_clause(&mut vec![1]),
+            mk_clause(&mut vec![-1])
+        ])));
+    }
+
+    #[test]
+    fn simple_unsat_2() {
+        assert!(!sat(mk_formula(&mut vec![
+            mk_clause(&mut vec![1]),
+            mk_clause(&mut vec![2]),
+            mk_clause(&mut vec![-1, -2]),
+        ])));
+    }
+
+    #[test]
+    fn complex_sat_7() {
+        assert!(sat(mk_formula(&mut vec![
+            mk_clause(&mut vec![-3, 4]),
+            mk_clause(&mut vec![-1, -3, -5]),
+            mk_clause(&mut vec![-2, -4, -5]),
+            mk_clause(&mut vec![-2, 3, 5, -6]),
+            mk_clause(&mut vec![-1, 2]),
+            mk_clause(&mut vec![-1, 3, -5, -6]),
+            mk_clause(&mut vec![1, -6]),
+            mk_clause(&mut vec![1, 7]),
+        ])));
+    }
+
+    fn mk_lit(atom: isize) -> Literal {
+        if atom < 0 {
+            Literal::new(Atom((atom * -1) as usize)).negate()
+        } else {
+            Literal::new(Atom(atom as usize))
+        }
+    }
+
+    fn mk_clause(lits: &mut Vec<isize>) -> Clause {
+        if let Some(a) = lits.pop() {
+            let mut clause = Clause::new(mk_lit(a));
+            while let Some(b) = lits.pop() {
+                clause.insert_literal(mk_lit(b));
+            }
+            clause
+        } else {
+            panic!("tried to construct empty clause")
+        }
+    }
+
+    fn mk_formula(clauses: &mut Vec<Clause>) -> Formula {
+        if let Some(a) = clauses.pop() {
+            let mut formula = Formula::new(a);
+            while let Some(b) = clauses.pop() {
+                formula.insert_clause(b);
+            }
+            formula
+        } else {
+            panic!("tried to construct empty formula")
+        }
     }
 }
